b5f5f80a8d3655f348ab9b513f1e395492336cf2
Miko Garcia <crymehonions@gmail.com>
2016-06-10 15:42:53 +0800

    Fixed redundant code

--- taskAdded.php
+++ taskAdded.php
@@ -1,6 +1,7 @@
 <html>
     <head>
         <title>Add Task</title>
+        <script src="http://www.w3schools.com/lib/w3data.js"></script>
     </head>
     <body>
         <?php
@@ -65,53 +66,11 @@
         }
         
         ?>
-        
-        <form action="taskAdded.php" method="post">
-        
-            <b>Add a new Task</b>
-            
-            <p>Task Name:
-            <input type="text" name="task_name" size="30" value=""/>
-            </p>
-            
-            <p>Task Category:
-            <select name="task_category">
-            <?php
-                require_once('mysqli_connect.php');
 
-                $query = "SELECT * FROM categories";
-                $response = @mysqli_query($dbc, $query);
-
-                while($row = mysqli_fetch_array($response)){
-                    echo    "<option value = " . $row['category_id'] . ">" .
-                            $row['category_name'] . "</option>";
-                }
-
-            ?>
-            </select>
-            </p>
-            
-            <p>Task Date:
-            <input type="date" name="task_date" size="30" value=""/>
-            </p>
-            
-            <p>Task Status:
-            <input type="number" name="task_Status" size="30" value=""/>
-            </p>
-            
-            <p>Task Content:
-            <input type="text" name="task_Content" size="30" value=""/>
-            </p>
-            
-            <!-- <p>Task Image:
-            <input type="text" name="task_Image" size="30" value=""/>
-            </p> -->
-            
-            <p>
-                <input type="submit" name="submit" value="Send" />
-            </p>
+        <div w3-include-html = "addTask.php"></div>
         
-            
-        </form>
+        <script>
+            w3IncludeHTML();
+        </script>
     </body>
 </html>
